017e116f95a00c0dae9fe461fbce6484cf744be9
DiHand79 <31386937+DiHand79@users.noreply.github.com>
2023-09-07 17:21:59 +0300

    1 file modified

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
+// import 'antd/dist/antd.css';
 import './index.css';
 import { Collapse } from 'antd';
 
 function CollapseBugSample(props) {
   const { Panel } = Collapse;
 
-  // let [expand, setExpand] = useState(false);
-  let expand = 'test bug';
+  let [expand, setExpand] = useState(false);
 
   function callback(key) {
     console.log(key);
-    // setExpand((prev) => !prev);
+    setExpand((prev) => !prev);
   }
 
   const text = `
@@ -22,55 +21,66 @@ function CollapseBugSample(props) {
   `;
   const linkANTDDoc = 'https://ant.design/components/collapse#collapsepanel';
 
+  function AsItWillWork() {
+    return (
+      <Collapse defaultActiveKey="1">
+        <Panel header="This is a nested panel" key="1">
+          <h3>{JSON.stringify(expand)}</h3>
+          <p>{text}</p>
+        </Panel>
+      </Collapse>
+    );
+  }
+
   return (
     <Collapse
-      // onChange={callback}
+      onChange={callback}
       items={[
         {
-          key: 'abc123',
-          label: <h3>test</h3>,
-          children: <a href={linkANTDDoc}>Doc link</a>,
+          key: 1,
+          label: <b>{'test label, Expand: ' + expand}</b>,
+          // header: 'test header',  // not work
+          children: <Panel header={AsItWillWork()} key="1"></Panel>,
+        },
+        {
+          key: 2,
+          // label: 'test label',
+          header: 'test header',
+          children: (
+            <>
+              <h3>{JSON.stringify(expand)}</h3>
+              <p>{text}</p>
+            </>
+          ),
+        },
+        {
+          key: 3,
+          label: 'test label',
+          // header: 'test header',
+          children: (
+            <Panel
+              header={
+                <>
+                  <h3>{JSON.stringify(expand)}</h3>
+                  <p>{text}</p>
+                </>
+              }
+              key="3"
+            ></Panel>
+          ),
+        },
+        {
+          key: 2,
+          label: 'test label',
+          // header: 'test header',
+          children: (
+            <Panel header="Bad Sample Work - NOT WoRk" key="4">
+              <h3>{JSON.stringify(expand)}</h3>
+              <p>{text}</p>
+            </Panel>
+          ),
         },
       ]}
-      // items={[
-      //   {
-      //     key: 1,
-      //     // label: 'test label',
-      //     // header: 'test header',
-      //     children: (
-      //       <Panel header="This is panel header 1" key="1">
-      //         <Collapse defaultActiveKey="1">
-      //           <Panel header="This is a nested panel" key="1">
-      //             <h3>{JSON.stringify(expand)}</h3>
-      //             <p>{text}</p>
-      //           </Panel>
-      //         </Collapse>
-      //       </Panel>
-      //     ),
-      //   },
-      //   {
-      //     key: 2,
-      //     // label: 'test label',
-      //     // header: 'test header',
-      //     children: (
-      //       <Panel header="This is panel header 2" key="2">
-      //         <h3>{JSON.stringify(expand)}</h3>
-      //         <p>{text}</p>
-      //       </Panel>
-      //     ),
-      //   },
-      //   {
-      //     key: 3,
-      //     // label: 'test label',
-      //     // header: 'test header',
-      //     children: (
-      //       <Panel header="This is panel header 3" key="3">
-      //         <h3>{JSON.stringify(expand)}</h3>
-      //         <p>{text}</p>
-      //       </Panel>
-      //     ),
-      //   },
-      // ]}
     />
   );
 }
